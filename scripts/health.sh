f15cea4c1e8fecfcff3cd4302e21007f11fdde95
dongchan1ee <ehdcks3421@gmail.com>
2024-02-08 03:02:27 +0900

    test해보고 안되면 자자,,

--- scripts/health.sh
+++ scripts/health.sh
@@ -2,8 +2,8 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
-source ${ABSDIR}/switch.sh
+source "${ABSDIR}/profile.sh"
+source "${ABSDIR}/switch.sh"
 
 IDLE_PORT=$(find_idle_port)
 
@@ -12,31 +12,25 @@ echo "> IDLE_PORT : $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
-for RETRY_COUNT in {1..10}
-do
+for RETRY_COUNT in {1..10}; do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+  UP_COUNT=$(echo "${RESPONSE}" | grep 'real' | wc -l)
 
-  if [ ${UP_COUNT} -ge 1 ]
-
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+  if [ "${UP_COUNT}" -ge 1 ]; then
     echo "> Health check 성공"
     switch_proxy
     break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check : ${RESPONSE}"
-
+    echo "> Health check 실패 또는 실행 중이 아님."
+    echo "> Health check 응답: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10 ]
-  then
-    echo "> Health check 실패"
+  if [ "${RETRY_COUNT}" -eq 10 ]; then
+    echo "> Health check 10회 실패"
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
   echo "> Health check 연결 실패. 재시도..."
-  sleep 10
-
+  sleep 15
 done
